83b7bfe75c8af869da886c7f2051d90532746ab9
ACHYUTH CHOWDARY D A <88281033+Achyuthc@users.noreply.github.com>
2022-05-08 09:51:59 +0530

    Create Flatten Nested List Iterator(LeetCode341).cpp

---
+++ Flatten Nested List Iterator(LeetCode341).cpp
@@ -0,0 +1,71 @@
+/*You are given a nested list of integers nestedList. Each element is either an integer or a list whose elements may also be integers or other lists. Implement an iterator to flatten it.
+
+Implement the NestedIterator class:
+
+NestedIterator(List<NestedInteger> nestedList) Initializes the iterator with the nested list nestedList.
+int next() Returns the next integer in the nested list.
+boolean hasNext() Returns true if there are still some integers in the nested list and false otherwise.
+Your code will be tested with the following pseudocode:*/
+
+
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * class NestedInteger {
+ *   public:
+ *     // Return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     bool isInteger() const;
+ *
+ *     // Return the single integer that this NestedInteger holds, if it holds a single integer
+ *     // The result is undefined if this NestedInteger holds a nested list
+ *     int getInteger() const;
+ *
+ *     // Return the nested list that this NestedInteger holds, if it holds a nested list
+ *     // The result is undefined if this NestedInteger holds a single integer
+ *     const vector<NestedInteger> &getList() const;
+ * };
+ */
+
+class NestedIterator {
+private:
+    stack<vector<NestedInteger>::iterator> begins, ends;
+public:
+    NestedIterator(vector<NestedInteger> &nestedList) {
+        begins.push(nestedList.begin());
+        ends.push(nestedList.end());
+    }
+    
+    int next() {
+        hasNext();
+        return (begins.top()++)->getInteger();
+    }
+    
+    bool hasNext() {
+        while(begins.size())
+        {
+            if(begins.top()==ends.top())
+            {
+                begins.pop();
+                ends.pop();
+            }
+            else
+            {
+                auto x=begins.top();
+                if(x->isInteger())
+                    return true;
+                begins.top()++;
+                begins.push(x->getList().begin());
+                ends.push(x->getList().end());
+                
+            }
+        }
+        return false;
+        
+    }
+};
+
+/**
+ * Your NestedIterator object will be instantiated and called as such:
+ * NestedIterator i(nestedList);
+ * while (i.hasNext()) cout << i.next();
+ */
